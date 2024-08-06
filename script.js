84387c07fa8841b45b23300a3b2790a4b35d3200
NicolasFue05 <nicolasfue05@gmail.com>
2024-08-06 17:32:27 -0500

    :feat: new files added

---
+++ script.js
@@ -0,0 +1,12 @@
+// Galeria de imagenes 
+
+const URLInput = document.querySelector('image-url');
+
+function addImage(url, title){
+    try{
+
+    }
+    catch(err){
+        console.error(err)
+    }
+}
